159a042a16fe14e420bfb6b85d482ea67bcc9a8a
Elman <200522elmanok>
2024-05-12 01:54:28 +0500

    Fiver

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import { Checkout } from './containers/Checkout/Checkout';
-import { NotFound } from './components/NotFound/NotFound';
-import { ContactData } from './containers/ContactData/ContactData';
-import { Layout } from './components/Layout/Layout';
-import { Orders } from './containers/Orders/Orders';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import { Checkout } from "./containers/Checkout/Checkout";
+import { NotFound } from "./components/NotFound/NotFound";
+import { ContactData } from "./containers/ContactData/ContactData";
+import { Layout } from "./components/Layout/Layout";
+import { Orders } from "./containers/Orders/Orders";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/burgers">
       <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index element={<BurgerBuilder/>}/>
-          <Route path='/orders' element={<Orders/>}/>
-          <Route path='/checkout' element={<Checkout/>}>
-            <Route path='contactData' element={<ContactData/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<BurgerBuilder />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/checkout" element={<Checkout />}>
+            <Route path="contactData" element={<ContactData />} />
           </Route>
-          <Route path='*' element={<NotFound/>}/>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
