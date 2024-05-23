de857c938f092e776d8424cc114d19a706533215
Elman <108092635+Elman2055@users.noreply.github.com>
2024-05-23 16:53:01 +0500

    Update axiosBurger.ts
    new link

--- src/config/axiosBurger.ts
+++ src/config/axiosBurger.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://burger-278a4-default-rtdb.firebaseio.com/";
+axios.defaults.baseURL = "https://burgers-ea0fb-default-rtdb.firebaseio.com/";
 
 const axiosBurger = axios;
 
